03bb2ee571d0265feaddd388c5eea06f29ebfcde
xin.liu <xin.liu@coding-fun.com>
2023-12-23 15:46:57 +0800

    some answers 00533 00898 03057 03060 03312

--- type-challenges/playground-231221/easy/00533-easy-concat.ts
+++ type-challenges/playground-231221/easy/00533-easy-concat.ts
@@ -18,19 +18,19 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Concat<T, U> = any
+type Concat<T extends readonly any[], U extends readonly any[]> = [...T, ...U]
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type {Equal, Expect} from '@type-challenges/utils'
 
 const tuple = [1] as const
 
 type cases = [
-  Expect<Equal<Concat<[], []>, []>>,
-  Expect<Equal<Concat<[], [1]>, [1]>>,
-  Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
-  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+    Expect<Equal<Concat<[], []>, []>>,
+    Expect<Equal<Concat<[], [1]>, [1]>>,
+    Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
+    Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+    Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
 ]
 
 // @ts-expect-error
